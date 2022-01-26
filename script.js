f5247f45487d021c36448a1a838eff039b2daedc
Gerimae Oliva <gerimae.o@gmail.com>
2022-01-25 22:57:16 -0500

    updated code in class

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+// Add items to the DOM
 const section = document.querySelector('section');
-const numberOfMoves = document.querySelector('span');
-const playerMoves = 7
-
-numberOfMoves.textContent = playerMoves
+const playerMoves = document.querySelector('span');
 
 // Cards Object
-const cardsArr = () => [
+const cardsData = () => [
     { name: 'iceCreamCat', imgSrc: './images/memory_game_card1.jpg' },
     { name: 'iceCreamCat', imgSrc: './images/memory_game_card1.jpg' },
     { name: 'cookieCat', imgSrc: './images/memory_game_card2.jpg' },
@@ -16,35 +14,58 @@ const cardsArr = () => [
     { name: 'instantLose', imgSrc: './images/memory_game_bonus_card2.jpg' },
     { name: 'instantMatch', imgSrc: './images/memory_game_bonus_card3.jpg' },
 ];
-// images.src = imgSrc
 
 // Card Randomization Function
 const randomize = () => {
-    const randomCard = cardsArr();
-    randomCard.sort(() => Math.random() - 0.5);
-    return randomCard;
+    const randomizedCards = cardsData();
+    randomizedCards.sort(() => Math.random() - 0.5);
+    return randomizedCards;
 };
-
 randomize();
 
 // Card Generator Funciton
 const cardGenerator = () => {
-    const randomCard = randomize();
-    randomCard.forEach(item => {
+    const randomizedCards = randomize();
+    randomizedCards.forEach(item => {
     const card = document.createElement('div');
     const face = document.createElement('img');
     const back = document.createElement('div');
     card.classList = 'card';
     face.classList = 'face';
     back.classList = 'back';
+    // Attach the images and names to the cards
     face.src = item.imgSrc;
+    card.setAttribute('name', item.name);
+    // Add the cards to DOM via Section
     section.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('toggleCard');
+        checkForMatches(e);
+        });
     });
 };
-
 cardGenerator();
+
+// Check for Matches
+const checkForMatches = (e) => {
+    const clickedCard = e.target;
+    clickedCard.classList.add('flipped');
+}
+
+// Player Moves
+
+
+// Event Listeners
+const newGameBtnEl = document.querySelector('.new-game');
+
+newGameBtnEl.addEventListener('click', () => {
+    location.reload();
+});
+
+
+
 
 // Write new round and/or new game functions
 // Write function to check for matched cards
