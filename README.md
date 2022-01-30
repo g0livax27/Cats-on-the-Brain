dab712e68338c08b922687a139a1ef7a538532ba
Gerimae Oliva <gerimae.o@gmail.com>
2022-01-30 00:17:45 -0500

    cleaned up CSS/JS code, removed lines of code that were not working

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Cats-on-the-Brain
 
-A memory game with a little twist.
+A Memory Game against the Computer.
 
-Cats on the Brain is a 2-player Memory Game where three rounds are played, and the best two out of three wins the game. At the end of each round, the cards will randomize to start the next round.
+You're given 5 moves and you must find all the matching cards before you run out of moves, otherwise you lose.
 
-What determines the winner of a round is the player who finds all matching cards with the least amount of tries. The twist is that there are three bonus cards in the mix. One bonus card is an Instant Winner card; if selected, the player automatically wins that round. Another bonus card is an Instant Loser card; if selected, the player automatically loses that round. And the last bonus card is a Wild Card; this card will automatically match with any card you select next (if chosen as the first card) or will automatically match with the card previously selected (if chosen as the second card).
+The twist is that there are three bonus cards in the mix:
+
+One bonus card is an Instant Winner card; if selected, you win.
+
+Another bonus card is an Instant Loser card; if selected, you lose.
+
+And the last bonus card is a Wild Card; this card will match with any card you select next (if chosen as the first card) or will match with the card previously selected (if chosen as the second card).
--- script.js
+++ script.js
@@ -1,40 +1,12 @@
 // Add items to the DOM
 const section = document.querySelector('section');
-const playerMovesCount = document.querySelector('span');
-const para1 = document.querySelector('#p1');
-const para2 = document.querySelector('#p2');
-let playerMoves = 0
+const movesCount = document.querySelector('span');
+let moves = 7
 
-playerMovesCount.textContent = playerMoves;
+/* */
 
-
-
-// Player Class
-class Player {
-    constructor() {
-        this.score = 0;
-        this.moves = 0;
-    }
-    // Player Moves Count Method (for loop? needs to iterate the number of clicks on a card & moveCount needs to reset at the end of each round)
-    // moveCount() {
-        // this.moves +=1; (or just do playerMoves++, but has to tie with the clickedCard())
-    // }
-    // Player Score Method (tracked via conditional of whether player wins or loses round)
-    // score() {
-        // this.score += 1;
-    // }
-}
-
-// Player 1 Instantiation
-const playerOne = new Player();
-// console.log(playerOne);
-
-// Player 2 Instantiation
-const playerTwo = new Player();
-// console.log(playerTwo);
-
-// Cards Object
-const cardsData = () => [
+// Cards Object/Array
+const cardData = () => [
     { name: 'iceCreamCat', imgSrc: './images/memory_game_card1.jpg' },
     { name: 'iceCreamCat', imgSrc: './images/memory_game_card1.jpg' },
     { name: 'cookieCat', imgSrc: './images/memory_game_card2.jpg' },
@@ -46,13 +18,55 @@ const cardsData = () => [
     { name: 'instantMatch', imgSrc: './images/memory_game_bonus_card3.jpg' },
 ];
 
+/* */
+
 // Card Randomization Function
 const randomize = () => {
-    const randomizedCards = cardsData();
+    const randomizedCards = cardData();
     randomizedCards.sort(() => Math.random() - 0.5);
     return randomizedCards;
 };
-randomize();
+// Randomizes Cards when page loads
+window.onload = randomize();
+
+/* */
+
+// const grid = document.querySelector('.grid');
+// const cardsChosen = [];
+// const cardsChosenId = [];
+// const cardsWon = [];
+
+// const createBoard = () => {
+
+// createBoard();
+
+// const flipCard = () => {
+//     let cardId = this.getAttribute('data-id');
+//     cardsChosen.push(cardData[cardId].name)
+//     cardsChosenId.push(cardId);
+//     this.setAttribute('src', cardData[cardId].img);
+//     if(cardsChosen.length === 2) {
+//         setTimeout(checkForMatch, 500)
+//     }
+// }
+
+// const checkForMatch = () => {
+//     const cards = document.querySelectorAll('img');
+//     let optionOneId = cardsChosenId[0];
+//     let optionTwoId = cardsChosenId[1];
+//     if(cardsChosen[0] === cardsChosen[1]) {
+//         console.log('Match');
+//         cards[optionOneId].setAttribute('src', './images/cardfront.jpg');
+//         cards[optionTwoId].setAttribute('src', './images/cardfront.jpg');
+//         cardsWon.push(cardsChosen);
+//     } else {
+//         cards[optionOneId].setAttribute('src', './images.cardback.jpg');
+//         cards[optionOneId].setAttribute('src', './images.cardback.jpg');
+//         console.log('Try Again')
+//     }
+//     cardsChosen = [];
+//     cardsChosenId = [];
+// }
 
 // Card Generator Funciton
 const cardGenerator = () => {
@@ -73,45 +87,18 @@ const cardGenerator = () => {
     section.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
-    // EventListener for when a card is clicked
-    card.addEventListener('click', () => {
-        card.classList.toggle('toggleCard');
-        checkForMatches();
+
+    // Event Listener for when a card is clicked
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('toggleCard') 
+        card.classList.add('flipped');
+        checkForMatches(e);
         });
-    });
+     });
 };
+cardGenerator();
 
-
-// Check for Matches
-const checkForMatches = () => {
-    const clickedCard = [];
-    // Add class of 'flipped' to cards so we can run a check for matches
-    // clickedCard.classList.add('flipped');
-    const flippedCards = []; //document.querySelectorAll('.flipped');
-    // If 2 cards are clicked
-    if(flippedCards.length === 2) {
-        // Check to see if the 1st card's name equals the 2nd card's name
-        if(flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name'));
-        // If yes, it's a match
-        // window.alert('You Found a Match');
-        // Remove the 'flipped' class from cards so card stays flipped up because it's a match, and they cannot be clicked again
-        flippedCards.forEach(card => {
-            // card.classList.remove('flipped');
-            card.style.pointerEvents = 'none';
-        })
-    } else {
-        // Else, it's not a match
-        // window.alert('Try Again!');
-        // Remove the 'flipped' and 'toggleCard' classes so other cards can be clicked and wrong cards flip back to be hidden
-        flippedCards.forEach(card => {
-            // card.classList.remove('flipped');
-            card.classList.remove('toggleCard');
-            // Set a delay for the cards to be flipped back
-            setTimeout(() => card.classList.remove('toggleCard'), 1000);
-            })
-        }
-    }
-    cardGenerator();
+/* */
 
 // Event Listeners
 const newGameBtnEl = document.querySelector('.new-game');
@@ -119,13 +106,3 @@ const newGameBtnEl = document.querySelector('.new-game');
 newGameBtnEl.addEventListener('click', () => {
     location.reload();
 });
-
-
-
-/*
-Write new round and/or new game functions
-Write function to check for matched cards
-Add if-else functions for if bonus cards are selected
-    Conditional for if 'Instant Match' card is selected
-Write a "Win" & "Lose" function
-*/
